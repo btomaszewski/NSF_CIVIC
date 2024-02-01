00272b312ac03099e033c77b708d71728b986eb9
Ben Baker <bensbaker35@gmail.com>
2024-02-01 09:31:30 -0500

    initial commit for deaf-map

---
+++ deaf-map/src/app/Map/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useEffect, useRef } from 'react';
+import ArcMap from '@arcgis/core/Map';
+
+export default function Map(){
+
+    const mapDiv = useRef(null);
+
+
+
+    return <p>This is a map</p>
+}
