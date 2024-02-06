7d07fe2b77dc3c831c74019a0f2a7c1166a4f412
Ben Baker <bensbaker35@gmail.com>
2024-02-06 11:30:10 -0500

    Basic Feature Layer & Feature Service

--- deaf-map/src/components/GIS/MapViewer.tsx
+++ deaf-map/src/components/GIS/MapViewer.tsx
@@ -1,26 +1,80 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, ReactElement } from "react";
 import Map from "@arcgis/core/Map";
 import WebMap from "@arcgis/core/WebMap";
 import MapView from "@arcgis/core/views/MapView";
 import config from "@arcgis/core/config";
+import ArcLayer from "@arcgis/core/layers/Layer";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 import "./MapViewer.css";
+import { Input } from "postcss";
 
-type MapViewerProps = {
+type LayerType = "FeatureLayer" | "FeatureService" | "VectorTileLayer";
+
+export interface BaseLayer {
+  url: string;
+  serviceType: LayerType;
+}
+
+export interface FeatureServer extends BaseLayer {
+  count: number;
+}
+//interface FeatureLayer extends BaseLayer {}
+
+type LayerInput = BaseLayer[] | BaseLayer;
+
+interface MapViewerProps {
   center?: Array<number>;
   APIKey?: string;
-};
+  InputLayers?: LayerInput;
+}
 
-export default function MapViewer({ center, APIKey }: MapViewerProps) {
+function LayerFactory(layers: LayerInput, map: Map) {
+  if (Array.isArray(layers)) {
+    for (let index = 0; index < layers.length; index++) {
+      const layer = layers[index];
+      LayerFactory(layer, map);
+    }
+  } else {
+    switch (layers.serviceType) {
+      case "FeatureLayer":
+        map.add(new FeatureLayer({ url: layers.url }));
+        break;
+      case "FeatureService":
+        let lServer: FeatureServer = layers as FeatureServer;
+        for (let i = 0; i < lServer.count; i++) {
+          //console.log(lServer.url + `${i}`);
+          map.add(new FeatureLayer({ url: lServer.url + `${i}` }), i);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+export default function MapViewer({
+  center,
+  APIKey,
+  InputLayers,
+}: MapViewerProps) {
   const mapDiv = useRef(null);
-  config.apiKey =
-    "AAPKbb819e3165fa4ab084c9b0fddef7a931Q9WxrEzWu5L3sOIM76FBUiKz7u67qoeCqwZvSCqsdh84t-MyCqu3lQJukCV-TIYN";
+  if (APIKey) {
+    config.apiKey = APIKey;
+  } else {
+    console.warn("No API Key defined, functionality might be reduced");
+  }
+
   useEffect(() => {
     if (mapDiv.current) {
       const map = new Map({
         basemap: "streets-vector",
       });
+
+      if (InputLayers) {
+        LayerFactory(InputLayers, map);
+      }
 
       const view = new MapView({
         container: mapDiv.current,
