a17448ca2bf5d9df9957e7160b9eb24bb52e7b8c
Ben Baker <bensbaker35@gmail.com>
2024-03-04 16:26:19 -0500

    A labeled input component

---
+++ deaf-map/src/components/LabeledInput/LabeledInput.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ChangeEventHandler, ReactElement } from "react";
+
+import "./LabeledInput.css";
+
+interface LabeledInputProps {
+  label: string;
+  pattern?: string;
+  value: string | number;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  type?: string;
+}
+
+export default function LabeledInput({
+  label,
+  pattern,
+  value,
+  onChange,
+  type,
+}: LabeledInputProps) {
+  return (
+    <div className="labeled-input-container">
+      <input onChange={onChange} pattern={pattern} value={value} type={type} />
+      <label>{label}</label>
+    </div>
+  );
+}
